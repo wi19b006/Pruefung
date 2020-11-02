55b2a0a9447a427770323cc0118812f4adcfac0e
wi19b006 <wi19b006@technikum-wien.at>
2020-11-02 18:53:17 +0100

    added feature file + step definitions

---
+++ Calculator/Calculator/CalculatorSteps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace Calculator
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        private double _number;
+        [Given(@"the number is Pi")]
+        public void GivenTheNumberIsPi()
+        {
+            _number = Math.PI;
+        }
+                
+        [Then(@"sin is (.*)")]
+        public void ThenSinIs(int p0)
+        {
+            var expected = p0;
+            Assert.Equal(expected, Math.Sin(_number),5);
+        }
+        
+        [Then(@"Cos is (.*)")]
+        public void ThenCosIs(int p0)
+        {
+            var expected = p0;
+            Assert.Equal(expected, Math.Cos(_number),5);
+        }
+        
+        [Then(@"tan is (.*)")]
+        public void ThenTanIs(int p0)
+        {
+            var expected = p0;
+            Assert.Equal(expected, Math.Tan(_number),5);
+        }
+    }
+}
---
+++ Calculator/Calculator/SpecFlowFeature1.feature
@@ -0,0 +1,18 @@
+﻿Feature: Calculator
+ In order to avoid silly mistakes
+ As a math noob
+ I want to use trigonometric functions
+
+@mytag
+
+ Scenario: CalcSin
+ Given the number is Pi
+ Then sin is 0
+
+Scenario: CalcCos
+ Given the number is Pi
+ Then Cos is -1
+
+Scenario: CalcTan
+ Given the number is Pi
+ Then tan is 0
